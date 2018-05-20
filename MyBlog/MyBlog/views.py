49fc91d07d8bc43557aa2f55df53f6c42990b54d
dhj0506 <dhj0506@gmail.com>
2018-05-20 14:50:56 +0800

    添加首页

--- MyBlog/MyBlog/views.py
+++ MyBlog/MyBlog/views.py
@@ -1,7 +1,9 @@
 # -*- encoding=UTF-8 -*-
 
-from MyBlog import app  # 导入app
+from MyBlog import app, db  # 导入app
+from models import Blog, User
+from flask import render_template, redirect, request
 
 @app.route('/')  # 首页
 def index():
-    return 'Hello'
+    return render_template('index.html')
