72c117073449e88ef4a2bb2799f4bdce6ca13090
dhj0506 <dhj0506@gmail.com>
2018-06-14 16:28:18 +0800

    读取数据库中的文章显示到首页

--- MyBlog/MyBlog/models.py
+++ MyBlog/MyBlog/models.py
@@ -6,19 +6,21 @@ from datetime import datetime
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 数据库中的一列，整数类型，主键，自增长
+    title = db.Column(db.String(160))
     content = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_date = db.Column(db.DateTime)
     status = db.Column(db.Integer, default=0)  # 0 正常 1删除
 
-    def __init__(self, content, user_id):
+    def __init__(self, title, content, user_id):
+        self.title = title
         self.content = content
         self.user_id = user_id
         self.created_date = datetime.now()  # 现在的时间
         # id是自动生成的
 
     def __repr__(self):  # 返回一个可以用来表示对象的可打印字符串
-        return '<Blog %d %s>' % (self.id, self.content)
+        return '<Blog %d %s %s>' % (self.id, self.title, self.content)
 
 
 class User(db.Model):
--- MyBlog/MyBlog/views.py
+++ MyBlog/MyBlog/views.py
@@ -4,6 +4,8 @@ from MyBlog import app, db  # 导入app
 from models import Blog, User
 from flask import render_template, redirect, request
 
+
 @app.route('/')  # 首页
 def index():
-    return render_template('index.html')
+    blogs = Blog.query.order_by(db.desc(Blog.id)).limit(10).all()
+    return render_template('index.html', blogs=blogs)
