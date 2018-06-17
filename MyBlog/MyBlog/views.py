b5fd8686e7d82b26cd0d62e582c13916cc3f9a7f
dhj0506 <dhj0506@gmail.com>
2018-06-17 20:20:27 +0800

    添加profile页面和otherProfile页面

--- MyBlog/MyBlog/views.py
+++ MyBlog/MyBlog/views.py
@@ -9,3 +9,9 @@ from flask import render_template, redirect, request
 def index():
     blogs = Blog.query.order_by(db.desc(Blog.id)).limit(10).all()
     return render_template('index.html', blogs=blogs)
+
+
+@app.route('/profile')  # 首页
+def profile():
+    blogs = Blog.query.order_by(db.desc(Blog.id)).limit(10).all()
+    return render_template('profile.html', blogs=blogs)
